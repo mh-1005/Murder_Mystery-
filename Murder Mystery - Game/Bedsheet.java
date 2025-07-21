76abb14a2267bc3d998e3122d34ed3d6064efd02
mh-1005 <maheen.1023@gmail.com>
2025-07-21 14:48:44 +0500

    Update Bedsheet.java

--- Murder Mystery - Game/Bedsheet.java
+++ Murder Mystery - Game/Bedsheet.java
@@ -1,5 +1,5 @@
 // Subclass of Item class
-// Bedhsheet is an item in the Crime Scene
+// Bedsheet is an item in the Crime Scene
 class Bedsheet extends Item{
     Bedsheet(String name,String description){
         super(name,description);
